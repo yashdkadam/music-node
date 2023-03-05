734c80c7797c9e8cf7e7887ba82c62d738267494
yashdkadam <kadamdyash55@gmail.com>
2023-03-05 15:51:54 +0530

    changed cors policy

--- startup/cors.js
+++ startup/cors.js
@@ -1,11 +1,5 @@
 const cors = require("cors");
 
-var corsOptions = {
-  origin: "https://movie-web-app-2002.web.app/",
-};
-
-
 module.exports = function(app) {
-  app.use(cors(corsOptions));
-  // app.use(cors());
+  app.use(cors());
 };
